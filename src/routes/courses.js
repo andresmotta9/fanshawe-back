16fa1d75b5fb0167feb7b71290664731012e79c5
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-04-02 20:39:24 -0400

    update response to camelCase

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,7 +4,17 @@ const pool = require('../config/db');
 
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM programs');
+    const result = await pool.query(`
+      SELECT
+        program_code AS "programCode",
+        name,
+        description,
+        top_skills AS "topSkills",
+        link,
+        jobs,
+        requirements
+      FROM programs
+    `);
     res.json(result.rows);
   } catch (error) {
     console.error('Error fetching courses:', error);
